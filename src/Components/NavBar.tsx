47e94526783dea056bdcbbc40a7303b5428c7aba
soitlaur <60842345+soitlaur@users.noreply.github.com>
2024-07-24 15:13:48 +0300

    Removed pointy arrow from last 3 elements of the navbar, made colored bar and text scale

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -6,15 +6,19 @@ import {ArrowBackIosNew} from "@mui/icons-material";
 const categories: string[] = ['Swords', 'Weapons', 'Armor', 'Shields', 'Clothing', 'Accessories',
     ' LARP Weapons & Gear', 'Sport Combat', 'Cultures', 'Brand', 'Sale'];
 
+const elementsToHide: number = 3;
+
 const NavBar: React.FC = () => {
     return (
         <Box className="box">
             {categories.map((category, index) => (
                 <Button className="category-button">
                     {category}
-                    <div className="arrow-div">
-                        <ArrowBackIosNew/>
-                    </div>
+                    {index < categories.length - elementsToHide && (
+                        <div className="arrow-div">
+                            <ArrowBackIosNew/>
+                        </div>
+                    )}
                 </Button>
             ))}
 
